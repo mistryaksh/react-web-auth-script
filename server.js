80b0f7ecc8c1020b0e24eca0c376249ec6042407
mistryaksh <mistryaksh1998@gmail.com>
2023-01-19 13:20:00 +0530

    ssl added

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(
 
 app.use(
      cors({
-          origin: ["http://localhost:3000/", "http://localhost:3000", "http://65.1.112.133:3000/", "http://65.1.112.133:3000"],
+          origin: ["http://localhost:3000/", "http://localhost:3000", "https://65.1.112.133:3000/", "https://65.1.112.133:3000"],
           optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
      })
 );
